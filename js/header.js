485cfe7b5f5f87f9b4cc7f57f6c0f1ad7c2a3de5
John Muñoz <jgmunozc@outlook.com>
2024-04-08 08:43:12 -0500

    Update header.js

--- js/header.js
+++ js/header.js
@@ -96,6 +96,14 @@ $(document).ready(function(){
     });
 });
 
+  //Codigo para volver a la página principal
+   
+   document.getElementById("#volver").addEventListener('click', function() {
+    $("Body>#overlay").css("display", "none");
+   });
+
+
+   
    
     /* Oprimir boton de enviar*/
 
